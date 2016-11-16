94bfddfbc330441e183cb8049eeeca271d5f7528
apyskir <andrzej.pyskir@student.uw.edu.pl>
2016-11-16 12:34:31 +0100

    *update makeInputList_AP.py

--- makeInputList_AP.py
+++ makeInputList_AP.py
@@ -11,23 +11,32 @@ fileList = os.listdir(inputDir)
 inputDir = inputDir.replace("/scratch/", "/cms/")
 initString = "inputFile = "
 
+outfile = open('inputFileList.txt','w')
+
 #MT
 for aFile in fileList:
     if aFile.find("MM_")!=-1 or aFile.find("TT_")!=-1: 
         continue
     initString+=inputDir+"/"+aFile+", "
+outfile.write(initString.rstrip(", ")+"\n")
+print initString
+initString = "inputFile = "
 
 #TT
 for aFile in fileList:
     if aFile.find("TT_")==-1: 
         continue
     initString+=inputDir+"/"+aFile+", "
+outfile.write(initString.rstrip(", ")+"\n")
+print initString
+initString = "inputFile = "
 
 #MT
 for aFile in fileList:
     if aFile.find("MM_")==-1: 
         continue
     initString+=inputDir+"/"+aFile+", "
-
-
+outfile.write(initString.rstrip(", ")+"\n")
 print initString
+
+outfile.close()
